ffae4d4a0965298ca49fc51ec5661697c5f56d09
nshakib <nazmus.shakib.cse@gmail.com>
2026-09-06 07:06:24 +0600

    refactor(category): add validation middleware

--- src/app/module/category/category.route.ts
+++ src/app/module/category/category.route.ts
@@ -2,14 +2,16 @@ import { Router } from "express";
 import { Role } from "../../../generated/prisma/enums";
 import { auth } from "../../middleware/checkAuth";
 import { CategoryController } from "./category.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createCategorySchema, updateCategorySchema } from "./category.validation";
 
 const router = Router();
 
 
-router.post('/', auth(Role.SUPER_ADMIN, Role.ADMIN), CategoryController.createCategory);
+router.post('/', auth(Role.SUPER_ADMIN, Role.ADMIN), validateRequest(createCategorySchema), CategoryController.createCategory);
 router.get('/', CategoryController.getAllCategories);
 router.get('/:id', CategoryController.getSingleCategory);
-router.patch('/:id', auth(Role.SUPER_ADMIN, Role.ADMIN), CategoryController.updateCategory);
+router.patch('/:id', auth(Role.SUPER_ADMIN, Role.ADMIN), validateRequest(updateCategorySchema), CategoryController.updateCategory);
 router.patch('/status/:id', auth(Role.SUPER_ADMIN, Role.ADMIN), CategoryController.updateCategoryStatus);
 
 export const CategoryRoutes = router;
